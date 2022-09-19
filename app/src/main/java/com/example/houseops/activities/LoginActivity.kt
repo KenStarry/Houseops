e6cb5e1d705244933f73ab43714dcffc8a89d640
KenStarry <starrycodes@gmail.com>
2022-09-19 22:19:20 +0300

    Login Activity completed and setup ViewBinding

--- app/src/main/java/com/example/houseops/activities/LoginActivity.kt
+++ app/src/main/java/com/example/houseops/activities/LoginActivity.kt
@@ -3,10 +3,23 @@ package com.example.houseops.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.houseops.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        listeners()
     }
+
+    private fun listeners() {
+
+        sign_up_text.setOnClickListener {
+
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/houseops/activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.houseops.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.houseops.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
