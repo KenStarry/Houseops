37ed78ad3efb905d08715b0bd60ebbe14011fbc8
KenStarry <starrycodes@gmail.com>
2022-09-27 00:09:21 +0300

    Redisgned Sign In Page UI

---
+++ app/src/main/java/com/example/houseops/activities/ForgotPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.houseops.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.houseops.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
