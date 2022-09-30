097c523bd7a22913067c1b1a398ff2eacc59b4da
KenStarry <starrycodes@gmail.com>
2022-10-01 01:23:10 +0300

    HouseOps redesigned the admin ui page

--- app/src/main/java/com/example/houseops/activities/AdminActivity.kt
+++ app/src/main/java/com/example/houseops/activities/AdminActivity.kt
@@ -3,7 +3,9 @@ package com.example.houseops.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.houseops.R
 import com.example.houseops.adapters.AdminCategoriesAdapter
 import com.example.houseops.databinding.ActivityAdminBinding
@@ -17,10 +19,10 @@ class AdminActivity : AppCompatActivity() {
     //  Houses List
     private val categories = listOf(
         AdminCategoriesModel("Singles", R.drawable.house1),
-        AdminCategoriesModel("Self-Contained", R.drawable.house1),
-        AdminCategoriesModel("Mansions", R.drawable.house1),
-        AdminCategoriesModel("Bedsitters", R.drawable.house1),
-        AdminCategoriesModel("One Bedroom", R.drawable.house1),
+        AdminCategoriesModel("Self-Contained", R.drawable.house2),
+        AdminCategoriesModel("Mansions", R.drawable.house5),
+        AdminCategoriesModel("Bedsitters", R.drawable.house3),
+        AdminCategoriesModel("One Bedroom", R.drawable.house4),
         AdminCategoriesModel("Two Bedroom", R.drawable.house1)
     )
 
@@ -29,6 +31,13 @@ class AdminActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //  Setting our toolbar
+        val toolbar: Toolbar = findViewById(R.id.adminToolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.title = "Admin"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         setupRecyclerView()
     }
@@ -39,7 +48,8 @@ class AdminActivity : AppCompatActivity() {
         adapter = AdminCategoriesAdapter(this@AdminActivity, categories)
         binding.adminRecyclerView.adapter = adapter
         binding.adminRecyclerView.layoutManager =
-            GridLayoutManager(this@AdminActivity, 2, GridLayoutManager.VERTICAL, false)
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+//            GridLayoutManager(this@AdminActivity, 2, GridLayoutManager.VERTICAL, false)
     }
 
 }
