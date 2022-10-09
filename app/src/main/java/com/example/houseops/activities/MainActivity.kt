7c644114c34f8b1ecac2202fe069fcebc5fffea3
KenStarry <starrycodes@gmail.com>
2022-10-09 18:38:58 +0300

    Added Fragment Transactions

--- app/src/main/java/com/example/houseops/activities/MainActivity.kt
+++ app/src/main/java/com/example/houseops/activities/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.core.content.ContextCompat
 import com.example.houseops.R
 import com.example.houseops.databinding.ActivityLoginBinding
 import com.example.houseops.databinding.ActivityMainBinding
+import com.example.houseops.fragments.BookedFragment
+import com.example.houseops.fragments.BookmarksFragment
+import com.example.houseops.fragments.HomeFragment
+import com.example.houseops.fragments.ProfileFragment
 import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.FirebaseAuth
@@ -34,19 +38,23 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         db = Firebase.firestore
         auth = Firebase.auth
 
+        listeners()
         changeStatusBarColor()
     }
 
-    private fun changeStatusBarColor() {
+    private fun listeners() {
 
-        this.window.apply {
-            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.accent_blue_dull)
-        }
+        binding.bottomNavigationView.setOnItemSelectedListener(this)
     }
+
 
     override fun onStart() {
         super.onStart()
+
+        //  Show the home fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragments_container, HomeFragment())
+            .commit()
 
         //  Watch the current user's activities
         val currentUser = auth.currentUser
@@ -54,9 +62,76 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         Toast.makeText(this, currentUser!!.email, Toast.LENGTH_SHORT).show()
     }
 
+
+    //  Override on back pressed method to prevent fragments from prepopulating
+    override fun onBackPressed() {
+
+        val count = supportFragmentManager.backStackEntryCount
+
+        if (count == 0)
+            super.onBackPressed()
+        else {
+            val fm = supportFragmentManager
+
+            fm.beginTransaction()
+                .replace(R.id.fragments_container, HomeFragment())
+                .commit()
+
+            //  Pop everything in the backstack
+            for (i in 0..fm.backStackEntryCount) {
+                fm.popBackStack()
+            }
+        }
+
+    }
+
+
+    //  The bottom navigation menu
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
-        when ()
+        when (item.itemId) {
+            R.id.home_menu -> {
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragments_container, HomeFragment())
+                    .commit()
+            }
+
+            R.id.booked_menu -> {
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragments_container, BookedFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+
+            R.id.bookmarks_menu -> {
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragments_container, BookmarksFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+
+            R.id.profile_menu -> {
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragments_container, ProfileFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+        return true
+    }
+
+
+    private fun changeStatusBarColor() {
+
+        this.window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.accent_blue_dull)
+        }
     }
 }
 
