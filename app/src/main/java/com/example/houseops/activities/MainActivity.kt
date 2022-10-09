d87d700f3a689782217c2ed62851da7aeb0f0965
KenStarry <starrycodes@gmail.com>
2022-10-09 17:56:57 +0300

    Redesigned Home Page

--- app/src/main/java/com/example/houseops/activities/MainActivity.kt
+++ app/src/main/java/com/example/houseops/activities/MainActivity.kt
@@ -5,19 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.MenuItem
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.houseops.R
 import com.example.houseops.databinding.ActivityLoginBinding
 import com.example.houseops.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarItemView
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var db: FirebaseFirestore
@@ -50,4 +53,19 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, currentUser!!.email, Toast.LENGTH_SHORT).show()
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when ()
+    }
 }
+
+
+
+
+
+
+
+
+
+
