b69cdfbf8f372e83361fad895f3777dc2cb68756
KenStarry <starrycodes@gmail.com>
2022-10-13 20:49:32 +0300

    Querying caretaker details from firebase to the caretaker activity

--- app/src/main/java/com/example/houseops/activities/CaretakerActivity.kt
+++ app/src/main/java/com/example/houseops/activities/CaretakerActivity.kt
@@ -10,14 +10,24 @@ import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.houseops.R
 import com.example.houseops.adapters.AdminCategoriesAdapter
+import com.example.houseops.collections.CaretakerCollection
 import com.example.houseops.databinding.ActivityCaretakerBinding
 import com.example.houseops.fragments.dialogs.AddHouseBottomSheet
 import com.example.houseops.models.AdminCategoriesModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class CaretakerActivity : AppCompatActivity() {
 
     private lateinit var adapter: AdminCategoriesAdapter
     private lateinit var binding: ActivityCaretakerBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     //  Houses List
     private val categories = listOf(
@@ -44,6 +54,47 @@ class CaretakerActivity : AppCompatActivity() {
 
         listeners()
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        auth = Firebase.auth
+        db = Firebase.firestore
+
+        val currentUser = auth.currentUser
+
+        //  Listen for changes on the caretaker's collection
+        queryCaretakerDetails(currentUser)
+    }
+
+    private fun queryCaretakerDetails(currentUser: FirebaseUser?) {
+
+        db.collection("caretakers")
+            .whereEqualTo("emailAddress", currentUser!!.email)
+            .limit(1)
+            .addSnapshotListener{querySnapshot, error ->
+
+                if (error != null)
+                    return@addSnapshotListener
+
+                //  Query the results
+                var apartment = ""
+                var name = ""
+                var image = ""
+
+                for (snapshot in querySnapshot!!) {
+
+                    val caretaker: CaretakerCollection = snapshot.toObject()
+
+                    apartment = caretaker.apartment
+                    name = caretaker.username
+                }
+
+                binding.caretakerName.text = name
+                binding.caretakerApartments.text= apartment
+
+            }
     }
 
     private fun listeners() {
